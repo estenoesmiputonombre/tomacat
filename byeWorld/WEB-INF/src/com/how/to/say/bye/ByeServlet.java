5f7006d2e3b33011f05ca7b5596719488ef5cc6d
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-08-22 13:22:19 +0200

    Changing the structure

---
+++ byeWorld/WEB-INF/src/com/how/to/say/bye/ByeServlet.java
@@ -0,0 +1,36 @@
+import com.how.to.say.bye;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ByeServlet extends HttpServlet {
+
+    static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+            out.println("<title>Hello, World</title></head>");
+            out.println("<body>");
+            out.println("<h1>Hello, world!</h1>"); // says Hello
+            // Echo client's request information
+            out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+            out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+            out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+            out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+            // Generate a random number upon each request
+            out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close(); // Always close the output writer
+        }
+    }
+
+}
--- byeWorld/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>Bye World!</h1>
-    </body>
-</html>
