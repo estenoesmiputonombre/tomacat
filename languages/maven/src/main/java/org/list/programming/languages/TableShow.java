b2cc896d4b5a86e68c1a786764fac7d7178dc47d
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-08-23 14:52:50 +0200

    Adding table show, just learning some suff before go to the weekend

--- languages/maven/src/main/java/org/list/programming/languages/TableShow.java
+++ languages/maven/src/main/java/org/list/programming/languages/TableShow.java
@@ -1,7 +1,15 @@
 package org.list.programming.languages;
 
-import javax.
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-public class TableShow extends HttpServ{
+public class TableShow extends HttpServe {
+
+    static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+
+    }
 
 }
